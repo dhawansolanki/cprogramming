c0ad2eb4b1f4601ddf39d688ce3a4cc744524d59
Dhawan Solanki <91565429+dhawansolanki@users.noreply.github.com>
2022-01-18 10:23:12 +0000

    Completed 0 and 1

--- p0original.c
+++ p0original.c
@@ -1,1 +1,8 @@
- 
+
+//0. Write a program to Print your name.
+
+#include <stdio.h>
+int main(){
+  printf("My Name Is Dhawan Solanki. \n");
+  return 0;
+}
--- p1original.c
+++ p1original.c
@@ -1,2 +1,14 @@
 
+//1. Write a program to Add two numbers
+
+#include <stdio.h>
+int main(){
+  int a,b,c;
+  printf("Enter First Number : ");
+  scanf("%d",&a);
+  printf("Enter Second Number : ");
+  scanf("%d",&b);
+  c=a+b;
+  printf("The Sum Of %d & %d is %d\n",a,b,c);
+  return 0;
 }
