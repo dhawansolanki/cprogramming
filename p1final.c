a36b3c744635d1d8a9c954738833684fcd4d5396
Dhawan Solanki <91565429+dhawansolanki@users.noreply.github.com>
2022-02-09 14:33:09 +0000

    Completed P1final.c

---
+++ p1final.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int a,b,c;
+printf("Enter First Number : ");
+scanf("%d", &a);
+printf("Enter Second Number : ");
+scanf("%d", &b);
+c = a + b;
+printf("The Sum of %d and %d is %d \n", a, b,c);
+return 0;
+}
