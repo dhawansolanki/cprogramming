c6dafd6e7982dbe29f6b08a9764f708610083f98
Dhawan Solanki <91565429+dhawansolanki@users.noreply.github.com>
2022-02-18 08:58:06 +0000

    Completed P0 and P1 Again

--- p1final.c
+++ p1final.c
@@ -1,11 +1,14 @@
+
+//1. Write a program to Add two numbers
+
 #include <stdio.h>
 int main(){
-int a,b,c;
-printf("Enter First Number : ");
-scanf("%d", &a);
-printf("Enter Second Number : ");
-scanf("%d", &b);
-c = a + b;
-printf("The Sum of %d and %d is %d \n", a, b,c);
-return 0;
+  int a,b,c;
+  printf("Enter First Number : ");
+  scanf("%d", &a);
+  printf("Enter Second Number : ");
+  scanf("%d", &b);
+  c = a + b;
+  printf("The Sum of %d and %d is %d \n", a, b,c);
+  return 0;
 }
---
+++ p2final.c
@@ -0,0 +1,34 @@
+// 2.	Write a program to add two number using 4 function	(pass by value)
+
+#include <stdio.h>
+int input()
+{
+    int a; 
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    return a;
+
+}
+
+
+int find_sum(int a, int b)
+{
+    int sum;
+    sum = a+b;
+    return sum;
+}
+
+void output(int a, int b, int c)
+{
+    printf("Sum of %d + %d is %d\n",a,b,c);
+}
+
+int main()
+{
+    int x,y,z;
+    x=input();
+    y=input();
+    z=find_sum(x,y);
+    output(x,y,z);
+    return 0;
+}
