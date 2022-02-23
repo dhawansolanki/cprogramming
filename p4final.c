cd9f351071c81f1920c663231bd0306d798797ed
Dhawan Solanki <91565429+dhawansolanki@users.noreply.github.com>
2022-02-23 09:26:44 +0000

    Completed P4

--- p4final.c
+++ p4final.c
@@ -1,16 +1,34 @@
-#include <stdio.h>
-
-int input(){
+#include<stdio.h>
+int input()
+{
   int a;
-  printf("Enter A Number : ");
-  scanf("%d", &a);
+  printf("enter the number:");
+  scanf("%d",&a);
   return a;
 }
-
-int cmp
-
-int main(){
-  int x;
-  x = input();
-  return 0;
+int cmp(int a,int b,int c)
+{
+  if(a>b&&a>c){
+    return a;
+  }
+  else if (b>a&&b>c){
+    return b;
+  }
+  else{
+    return c;
+  }
+}
+void output(int a,int b,int c,int largest)
+{
+  printf("the greatest number of %d,%d,%d is %d",a,b,c,largest);
+}
+int main()
+{
+  int x,y,z,largets;
+  x=input();
+  y=input();
+  z=input();
+  largets=cmp(x,y,z);
+  output(x,y,z,largets);
+  
 }
