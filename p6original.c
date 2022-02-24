368ffbdce2e779197f6a91d2026c6393cee574a0
Dhawan Solanki <91565429+dhawansolanki@users.noreply.github.com>
2022-02-24 11:27:49 +0000

    Tried p6original.c

--- p6original.c
+++ p6original.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+int input()
+{
+	int n;
+  printf("Enter n \n");
+	scanf("%d",&n);
+	return n;
+}
 
-int input_n(){
-  int a;
-  printf("Enter The Number : ");
+int sum_n(int n){
+	int sum = 0;
+	for( int i=1; i<=n; i++) 
+		sum += i;
+	return sum;
+}
+
+void output(int n, int sum)
+{
+	for( int i=1; i<n; i++) 
+		printf("%d + ",i);
+	printf("%d = %d",n,sum);
+}
+
+int main()
+{
+	int n = input();
+	int sum = sum_range(n);
+	output(n,sum);
+	return 0;
 }
